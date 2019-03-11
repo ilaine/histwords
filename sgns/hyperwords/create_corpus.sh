b6425680bcb2609d97f718779068d3af93d5a3f4
ilaine <wang.ilaine@gmail.com>
2019-03-11 15:28:27 +0100

    Add create_corpus
    Simple bash script to launch corpus2sgns.sh on the whole corpus at once

---
+++ sgns/hyperwords/create_corpus.sh
@@ -0,0 +1,15 @@
+bash corpus2sgns.sh --cds 0.75 --win 5 --neg 15 --dim 300 files/coha.1860s output/1860
+bash corpus2sgns.sh --cds 0.75 --win 5 --neg 15 --dim 300 files/coha.1870s output/1870
+bash corpus2sgns.sh --cds 0.75 --win 5 --neg 15 --dim 300 files/coha.1880s output/1880
+bash corpus2sgns.sh --cds 0.75 --win 5 --neg 15 --dim 300 files/coha.1890s output/1890
+bash corpus2sgns.sh --cds 0.75 --win 5 --neg 15 --dim 300 files/coha.1900s output/1900
+bash corpus2sgns.sh --cds 0.75 --win 5 --neg 15 --dim 300 files/coha.1910s output/1910
+bash corpus2sgns.sh --cds 0.75 --win 5 --neg 15 --dim 300 files/coha.1920s output/1920
+bash corpus2sgns.sh --cds 0.75 --win 5 --neg 15 --dim 300 files/coha.1930s output/1930
+bash corpus2sgns.sh --cds 0.75 --win 5 --neg 15 --dim 300 files/coha.1940s output/1940
+bash corpus2sgns.sh --cds 0.75 --win 5 --neg 15 --dim 300 files/coha.1950s output/1950
+bash corpus2sgns.sh --cds 0.75 --win 5 --neg 15 --dim 300 files/coha.1960s output/1960
+bash corpus2sgns.sh --cds 0.75 --win 5 --neg 15 --dim 300 files/coha.1970s output/1970
+bash corpus2sgns.sh --cds 0.75 --win 5 --neg 15 --dim 300 files/coha.1980s output/1980
+bash corpus2sgns.sh --cds 0.75 --win 5 --neg 15 --dim 300 files/coha.1990s output/1990
+bash corpus2sgns.sh --cds 0.75 --win 5 --neg 15 --dim 300 files/coha.2000s output/2000
