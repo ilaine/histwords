3b308a0620cbfcd17c93f88566036851cd660a41
ilaine <wang.ilaine@gmail.com>
2020-11-04 00:15:37 +0100

    Update postprocesssgns.py

--- sgns/postprocesssgns.py
+++ sgns/postprocesssgns.py
@@ -1,6 +1,16 @@
 import numpy as np
+import os, sys
 from multiprocessing import Queue, Process
 from argparse import ArgumentParser
+
+tpath = os.path.dirname(os.path.realpath(__file__))
+VIZ_DIR=os.path.join(tpath, "web")
+
+ROOT_DIR=tpath
+tpath = os.path.abspath(os.path.join(tpath, "../"))
+
+sys.path.append(tpath)
+os.chdir(tpath)
 
 from ioutils import load_pickle, write_pickle
 
@@ -9,11 +19,12 @@ def worker(proc_num, queue, dir, count_dir, min_count):
         if queue.empty():
             break
         year = queue.get()
-        print "Loading data..", year
+        print "Loading data...", year
+        print "Path..", "sgns/" + count_dir + str(year) + "-counts.pkl"
 #        time.sleep(120 * random.random())
-        freqs = load_pickle(count_dir + str(year) + "-counts.pkl")
+        freqs = load_pickle("sgns/" + count_dir + str(year) + "-counts.pkl")
         iw = []
-        with open(dir + str(year) + "-w.txt") as fp:
+        with open("sgns/" + dir + str(year) + "/" + str(year) + ".sgns.words.txt") as fp: # seems to correspond to X.counts.words.vocab
             info = fp.readline().split()
             vocab_size = int(info[0])
             dim = int(info[1])
@@ -24,25 +35,25 @@ def worker(proc_num, queue, dir, count_dir, min_count):
                 if freqs[iw[-1]] >= 500:
                     w_mat[i,:] = np.array(map(float, line[1:]))
         c_mat = np.zeros((vocab_size, dim))
-        with open(dir + str(year) + "-c.txt") as fp:
+        with open("sgns/" + dir + str(year) + "/" + str(year) + ".sgns.contexts.txt") as fp: # seems to correspond to X.counts.contexts.vocab >> TEST IN "GROUP"
             fp.readline()
             for i, line in enumerate(fp):
                 line = line.strip().split()
                 if freqs[line[0]] >= min_count:
                     c_mat[i,:] = np.array(map(float, line[1:]))
-        np.save(dir + str(year) + "-w.npy", w_mat)
-        np.save(dir + str(year) + "-c.npy", c_mat)
-        write_pickle(iw, dir + str(year) + "-vocab.pkl")
+        np.save("sgns/" + dir + str(year) + "/" + str(year) + "-w.npy", w_mat)
+        np.save("sgns/" + dir + str(year) + "/" + str(year) + "-c.npy", c_mat)
+        write_pickle(iw, "sgns/" + dir + str(year) + "/" + str(year) + "-vocab.pkl")
 
 if __name__ == "__main__":
     parser = ArgumentParser("Post-processes SGNS vectors to easier-to-use format. Removes infrequent words.")
     parser.add_argument("dir")
     parser.add_argument("count_dir", help="Directory with count data.")
     parser.add_argument("--workers", type=int, help="Number of processes to spawn", default=20)
-    parser.add_argument("--start-year", type=int, default=1800)
+    parser.add_argument("--start-year", type=int, default=1860) #
     parser.add_argument("--end-year", type=int, default=2000)
-    parser.add_argument("--year-inc", type=int, default=1)
-    parser.add_argument("--min-count", type=int, default=500)
+    parser.add_argument("--year-inc", type=int, default=10) #
+    parser.add_argument("--min-count", type=int, default=300) #
     args = parser.parse_args()
     years = range(args.start_year, args.end_year + 1, args.year_inc)
     queue = Queue()
